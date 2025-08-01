882b3ec4eed44dc0b6a7191c6935afc0e49205d4
fleven <franz.leven@kickstartds.com>
2025-08-01 13:07:57 +0200

    feat(auto): develop page

--- src/pages/Auto.stories.tsx
+++ src/pages/Auto.stories.tsx
@@ -6,9 +6,15 @@ import { Section } from "../components/section/SectionComponent";
 
 import { Text } from "../components/text/TextComponent";
 import { Hero } from "../components/hero/HeroComponent";
-import { ImageText } from "../components/image-text/ImageTextComponent";
 import { SplitEven } from "../components/split-even/SplitEvenComponent";
 import { Picture } from "@kickstartds/base/lib/picture";
+import { Slider } from "../components/slider/SliderComponent";
+import { Mosaic } from "../components/mosaic/MosaicComponent";
+import { Headline } from "@kickstartds/base/lib/headline";
+import { TextField } from "@kickstartds/form/lib/text-field";
+import { TextArea } from "@kickstartds/form/lib/text-area";
+import { CheckboxGroup } from "@kickstartds/form/lib/checkbox-group";
+import { Button } from "@kickstartds/base/lib/button";
 
 const Page = () => (
   <>
@@ -31,7 +37,6 @@ const Page = () => (
       />
     </Section>
     <Section
-      backgroundColor="accent"
       width="wide"
       headline={{
         align: "center",
@@ -82,6 +87,114 @@ const Page = () => (
           />
         }
       />
+    </Section>
+    <Section
+      spaceAfter="none"
+      headline={{
+        align: "center",
+        width: "default",
+        text: "Verbinden Sie sich nahtlos mit Ihrer Welt",
+        sub: "Entdecken Sie die Größe und Geräumigkeit dieses smarten Autos.",
+      }}
+    >
+      <Text
+        text="Mit seiner hochwertigen Innenausstattung sowie einem geräumigen und vielseitigen Kofferraum ist der neue Nissan Juke für Sie und Ihre Fahrten gemacht."
+        highlightText
+        align="center"
+      />
+    </Section>
+    <Section spaceBefore="small" width="max">
+      <Slider autoplay arrows>
+        <Hero
+          image={{
+            srcMobile:
+              "/guenther/nissan/24TDIEULHD_JUKE_MC_Lifestyle_pace-702.jpg.ximg_.l_12_h.smart_.jpg",
+          }}
+        />
+        <Hero
+          image={{
+            srcMobile:
+              "/guenther/nissan/24TDIEULHD_JUKE_MC_006-pace204.jpg.ximg_.l_12_h.smart_.jpg",
+          }}
+        />
+        <Hero
+          image={{
+            srcMobile:
+              "/guenther/nissan/24TDIEULHD_JUKE_MC_009-pace205.jpg.ximg_.l_12_h.smart_.jpg",
+          }}
+        />
+      </Slider>
+    </Section>
+    <Section
+      transition="to-accent"
+      width="wide"
+      headline={{
+        text: "Starker Klang in aufregender Klarheit mit BOSE® PERSONAL® PLUS",
+        align: "center",
+        width: "default",
+      }}
+    >
+      <Mosaic
+        layout="alternate"
+        tile={[
+          {
+            headline: "Das ist Musik in Ihren Ohren, nur noch besser",
+            image: {
+              src: "/guenther/nissan/24TDIEURHD_JUKE_MC_019-pace307.jpg.ximg_.l_6_h.smart_.jpg",
+            },
+            text: "Rock-, Pop- und Jazzmusik haben eines gemeinsam: exzellente Klangqualität. Das Bose® Personal® Plus¹ Soundsystem im neuen Nissan Juke verfügt über 10 leistungsstarke Lautsprecher, die strategisch im Fahrzeuginnenraum positioniert sind, um eine optimale Klangqualität zu gewährleisten.",
+          },
+          {
+            headline:
+              "Ein Platz in der ersten Reihe hat sich noch nie so gut angehört.",
+            image: {
+              src: "/guenther/nissan/24TDIEULHD_JUKE_MC_025-pace308.jpg.ximg_.l_6_h.smart_.jpg",
+            },
+            text: "Die Kopfstützen des Fahrer- und Beifahrersitzes im neuen Juke können als Teil des Bose® Personal® Plus Soundsystems mit Ultra-Nearfield-Lautsprechern ausgestattet werden.¹ Diese Lautsprecher ermöglichen Ihnen ein besonders intensives Eintauchen in Ihre Lieblingsmusik.",
+          },
+        ]}
+      />
+    </Section>
+    <Section content={{ mode: "list" }} style="framed">
+      <Headline text={"Kontaktformular"} level={"h3"} />
+      <SplitEven
+        contentMinWidth="narrow"
+        horizontalGutter="small"
+        first={<TextField required label="Vorname" />}
+        second={<TextField required label="Nachname" />}
+      />
+      <TextField required label="E-Mail" />
+      <TextField
+        required
+        label="Telefonnummer (optional)"
+        inputMode="tel"
+        type="tel"
+        placeholder="0123 4567890"
+      />
+      <TextArea required label="Kommentar oder Nachricht" />
+      <CheckboxGroup
+        label="Bitte wählen Sie eine Filiale"
+        options={[
+          {
+            label: "Hamburg Hamm",
+          },
+          {
+            label: "Hamburg Bergsdorf",
+          },
+          {
+            label: "Hamburg Farmsen",
+          },
+          {
+            label: "Hamburg Poppenbüttel",
+          },
+          {
+            label: "Ahrensburg",
+          },
+        ]}
+      />
+      <div>
+        <Button variant="primary" label="Anfrage Senden" />
+      </div>
     </Section>
 
     <Footer {...footerProps} inverted />
